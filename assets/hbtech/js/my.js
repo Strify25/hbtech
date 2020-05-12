5d4bd97b03c4946b7ee55b0223cda3c36e7dd434
Strify25 <mozhartsevr@gmail.com>
2020-05-12 18:15:32 +0300

    сделал виды работ

--- assets/hbtech/js/my.js
+++ assets/hbtech/js/my.js
@@ -2,18 +2,18 @@ jQuery(document).ready(function($) {
 	$('.menuBg').each(function(index, el) {
 		$(this).height($(this).parent("ul").height()+100);
 	});
+
 	$(document).on('click', '.openSearch', function(event) {
 		event.preventDefault();
 		
 		$('.searchWrap').toggleClass('active').find('input').focus();
 	});
 
+	$(".imageDots a").hover(function() {
+		var parent = $(this).parents(".uk-slideshow");
+		var index = $(this).parent().attr("uk-slideshow-item");
+		UIkit.slideshow(parent).show(index);
+	});
 
-
-$(".imageDots a").hover(function() {
-	var parent = $(this).parents(".uk-slideshow");
-	var index = $(this).parent().attr("uk-slideshow-item");
-	UIkit.slideshow(parent).show(index);
-});
 	
 });
