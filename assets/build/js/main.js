bea093c7972ab2ddbb0948e5f9328669088d77ee
Strify25 <mozhartsevr@gmail.com>
2020-05-18 22:02:49 +0300

    сделал адаптив

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -11090,10 +11090,28 @@ jQuery(document).ready(function($) {
 		UIkit.slideshow(parent).show(index);
 	});
 
-	if ($(window).width() > 639) {
+	if ($(window).width() > 960) {
 		UIkit.slider("#sertificatesSlider", {draggable:false});
 		UIkit.slider("#clientsSlider", {draggable:false});
 	}
+
+	if ($(window).width() < 1050) {
+		$('.mainNav').remove();
+
+		$.ajax({
+			url: '/mobileHeader.html',
+			dataType: 'html',
+		}).done(function(res) {
+			$('.mobileHeaderWrap').append(res);
+		});
+		
+	}
+
+	$(window).resize(function(event) {
+		if ($(window).width() < 1050) {
+			$('.mainNav').remove();
+		}
+	});
 
 	$("#callMeForm").validate({
             rules: {
@@ -11118,8 +11136,8 @@ jQuery(document).ready(function($) {
             errorClass: "uk-form-danger",
             validClass: "uk-form-success",
             submitHandler: function (form) {
-            	$('.callMeWrap').toggleClass('uk-hidden');
-            	$('.callMeOk').toggleClass('uk-hidden');
+            	$('.callMeWrap').addClass('uk-hidden');
+            	$('.callMeOk').removeClass('uk-hidden');
                 // $.ajax({
                 //     url: path,
                 //     type: 'POST',
@@ -11135,8 +11153,16 @@ jQuery(document).ready(function($) {
     );
 
     UIkit.util.on("#callMe", 'hidden', function() {
-            	$('.callMeWrap').toggleClass('uk-hidden');
-            	$('.callMeOk').toggleClass('uk-hidden');
+    	$('.callMeWrap').removeClass('uk-hidden');
+    	$('.callMeOk').addClass('uk-hidden');
+    });
+
+    UIkit.util.on("#callMe", 'show', function() {
+    	$(".mobile_menu_container, .mobile_sidebar_container").removeClass("loaded");
+        $(".mobile_menu_overlay").fadeOut(function() {
+            $(".mobile_menu_container .loaded").removeClass("loaded");
+            $(".mobile_menu_container .activity").removeClass("activity");
+        })
     });
 
 	$(window).scroll(function() {
@@ -11147,7 +11173,43 @@ jQuery(document).ready(function($) {
 		}
 	});
 
-	var html5Slider = document.getElementById('html5');
+	$(document).on("click", ".mobile_menu_container .parentMobileWrap span", function(e) {
+        e.preventDefault();
+        $(".mobile_menu_container .activity").removeClass("activity");
+        $(this).parent().siblings("ul").addClass("loaded").addClass("activity");
+    });
+    $(document).on("click", ".mobile_menu_container .back", function(e) {
+        e.preventDefault();
+        $(".mobile_menu_container .activity").removeClass("activity");
+        $(this).parent().parent().removeClass("loaded");
+        $(this).parent().parent().parent().parent().addClass("activity");
+    });
+    $(document).on("click", ".mobileMenuToggle", function(e) {
+        e.preventDefault();
+        $(".mobile_menu_container").addClass("loaded");
+        $(".mobile_menu_overlay").fadeIn();
+    });
+    $(document).on("click", ".mobileContactsToggle", function(e) {
+        e.preventDefault();
+        $(".mobile_sidebar_container").addClass("loaded");
+        $(".mobile_menu_overlay").fadeIn();
+    });
+    $(document).on("click", ".mobile_menu_overlay, .closeMobileMenu", function(e) {
+        $(".mobile_menu_container, .mobile_sidebar_container, .mobile_filter_container").removeClass("loaded");
+        $(".mobile_menu_overlay").fadeOut(function() {
+            $(".mobile_menu_container .loaded").removeClass("loaded");
+            $(".mobile_menu_container .activity").removeClass("activity");
+        })
+    })
+
+    $(document).on('click', '.mobileFilterBtn', function(event) {
+    	event.preventDefault();
+        $(".mobile_menu_container").removeClass("loaded");
+        $(".mobile_filter_container").addClass("loaded");
+    });
+
+
+	var html5Slider = document.getElementById('square');
 	noUiSlider.create(html5Slider, {
 	    start: [10, 30],
 	    connect: true,
@@ -11212,6 +11274,73 @@ jQuery(document).ready(function($) {
 	});
 	inputNumber.addEventListener('change', function () {
 	    widthHouse.noUiSlider.set([null, this.value]);
+	});
+
+	var html5SliderMobile = document.getElementById('squareMobile');
+	noUiSlider.create(html5SliderMobile, {
+	    start: [10, 30],
+	    connect: true,
+	    range: {
+	        'min': 0,
+	        'max': 30
+	    }
+	});
+	var inputNumberMobile = document.getElementById('input-numberMobile');
+	var inputNumber2Mobile = document.getElementById('input-number2Mobile');
+	html5SliderMobile.noUiSlider.on('update', function (values, handle) {
+
+	    var value = values[handle];
+
+	    inputNumberMobile.value = values[0];
+	    inputNumber2Mobile.value = values[1];
+	});
+	inputNumberMobile.addEventListener('change', function () {
+	    html5SliderMobile.noUiSlider.set([null, this.value]);
+	});
+
+	var lengthHouseMobile = document.getElementById('lengthHouseMobile');
+	noUiSlider.create(lengthHouseMobile, {
+	    start: [10, 30],
+	    connect: true,
+	    range: {
+	        'min': 0,
+	        'max': 30
+	    }
+	});
+	var inputlengthHouseMobile = document.getElementById('input-lengthHouseMobile');
+	var inputlengthHouse2Mobile = document.getElementById('input-lengthHouse2Mobile');
+	lengthHouseMobile.noUiSlider.on('update', function (values, handle) {
+
+	    var value = values[handle];
+
+	    inputlengthHouseMobile.value = values[0];
+	    inputlengthHouse2Mobile.value = values[1];
+	});
+	inputNumberMobile.addEventListener('change', function () {
+	    lengthHouseMobile.noUiSlider.set([null, this.value]);
+	});
+
+	
+	var widthHouseMobile = document.getElementById('widthHouseMobile');
+	noUiSlider.create(widthHouseMobile, {
+	    start: [10, 30],
+	    connect: true,
+	    range: {
+	        'min': 0,
+	        'max': 30
+	    }
+	});
+	var inputwidthHouseMobile = document.getElementById('input-widthHouseMobile');
+	var inputwidthHouse2Mobile = document.getElementById('input-widthHouse2Mobile');
+	widthHouse.noUiSlider.on('update', function (values, handle) {
+
+	    var value = values[handle];
+
+	    inputwidthHouseMobile.value = values[0];
+	    inputwidthHouse2Mobile.value = values[1];
+	});
+	inputNumberMobile.addEventListener('change', function () {
+	    widthHouseMobile.noUiSlider.set([null, this.value]);
 	});
 	
 });
